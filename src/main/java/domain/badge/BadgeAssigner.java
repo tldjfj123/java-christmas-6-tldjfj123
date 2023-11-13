468575593465a80f388d49245cd8b946a46ad6fd
Seongjun Ahn <dksckd0804@naver.com>
2023-11-13 22:41:04 +0900

    refactor : removed hard-coding elemens

--- src/main/java/domain/badge/BadgeAssigner.java
+++ src/main/java/domain/badge/BadgeAssigner.java
@@ -4,20 +4,25 @@ public class BadgeAssigner {
     private static final int STAR_THRESHOLD = 5_000;
     private static final int TREE_THRESHOLD = 10_000;
     private static final int SANTA_THRESHOLD = 20_000;
+    private static final String SANTA = "산타";
+    private static final String TREE = "나무";
+    private static final String STAR = "별";
+    private static final String NONE = "없음";
+
 
     public Badge assignBadge(int totalBenefitAmount) {
         if (totalBenefitAmount >= SANTA_THRESHOLD) {
-            return new Badge("산타");
+            return new Badge(SANTA);
         }
 
         if (totalBenefitAmount >= TREE_THRESHOLD) {
-            return new Badge("트리");
+            return new Badge(TREE);
         }
 
         if (totalBenefitAmount >= STAR_THRESHOLD) {
-            return new Badge("별");
+            return new Badge(STAR);
         }
 
-        return new Badge("없음");
+        return new Badge(NONE);
     }
 }
