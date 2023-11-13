65d43e5bea716824800a50d1794bba9b89666adb
Seongjun Ahn <dksckd0804@naver.com>
2023-11-13 23:35:22 +0900

    feat : implement validate method

--- src/main/java/domain/calendar/Date.java
+++ src/main/java/domain/calendar/Date.java
@@ -1,14 +1,29 @@
 package domain.calendar;
 
+import exception.InvalidNumberInputException;
+
 public class Date {
+    private static final int MIN_DATE = 1;
+    private static final int MAX_DATE = 31;
     private int date;
     private boolean hasStar;
     private boolean isWeekend;
 
     public Date(int date, boolean hasStar, boolean isWeekend) {
+        validate(date);
         this.date = date;
         this.hasStar = hasStar;
         this.isWeekend = isWeekend;
+    }
+
+    private void validate(int date) {
+        validateDateRange(date);
+    }
+
+    private void validateDateRange(int date) {
+        if (!(MIN_DATE <= date && date <= MAX_DATE)) {
+            throw new InvalidNumberInputException();
+        }
     }
 
     public int getDate() {
