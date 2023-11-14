f5080ccbe1a1c1d797e34872b2f2d4054b495eee
Seongjun Ahn <dksckd0804@naver.com>
2023-11-15 00:21:52 +0900

    fix : fixed wrong typed message

--- src/main/java/exception/InvalidOrderInputException.java
+++ src/main/java/exception/InvalidOrderInputException.java
@@ -1,7 +1,7 @@
 package exception;
 
 public class InvalidOrderInputException extends IllegalArgumentException {
-    private static final String ERROR_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요";
+    private static final String ERROR_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
 
     public InvalidOrderInputException() {
         super(ERROR_MESSAGE);
