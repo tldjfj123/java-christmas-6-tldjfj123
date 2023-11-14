6eff679e22251fdb5c1d6ec5033d56eecee08ef2
Seongjun Ahn <dksckd0804@naver.com>
2023-11-14 22:26:57 +0900

    feat : implement WelcomeView

---
+++ src/main/java/view/WelcomeView.java
@@ -0,0 +1,8 @@
+package view;
+
+public class WelcomeView {
+    private static final String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public void welcome() {
+        System.out.println(WELCOME_MESSAGE);
+    }
+}
