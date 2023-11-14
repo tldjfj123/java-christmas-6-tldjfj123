8778d25132caa77d9ce8c720406d487a00a691da
Seongjun Ahn <dksckd0804@naver.com>
2023-11-15 00:24:05 +0900

    feat : implement Application

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,24 @@
 package christmas;
 
+import controller.CalendarController;
+import controller.OrderController;
+import controller.PreviewController;
+import domain.calendar.Date;
+import domain.order.OrderList;
+import view.WelcomeView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        WelcomeView welcomeView = new WelcomeView();
+        welcomeView.welcome();
+
+        CalendarController calendarController = new CalendarController();
+        Date date = calendarController.run();
+
+        OrderController orderController = new OrderController();
+        OrderList orderList = orderController.run();
+
+        PreviewController previewController = new PreviewController();
+        previewController.run(date, orderList);
     }
 }
