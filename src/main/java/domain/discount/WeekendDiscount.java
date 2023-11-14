a60e6beaf9f65babc1aa53e00e843594f289b695
Seongjun Ahn <dksckd0804@naver.com>
2023-11-14 15:43:30 +0900

    refactor : convert method to static

--- src/main/java/domain/discount/WeekendDiscount.java
+++ src/main/java/domain/discount/WeekendDiscount.java
@@ -3,7 +3,7 @@ package domain.discount;
 public class WeekendDiscount {
     private static final int DISCOUNT_AMOUNT = 2023;
 
-    public int calculateDiscount(int quantity) {
+    public static int calculateDiscount(int quantity) {
         return DISCOUNT_AMOUNT * quantity;
     }
 }
