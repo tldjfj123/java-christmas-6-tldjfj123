2f4a86f0470eef9723b1869c067465111d490b48
Seongjun Ahn <dksckd0804@naver.com>
2023-11-14 22:02:23 +0900

    feat : implement OrderController

--- src/main/java/controller/OrderController.java
+++ src/main/java/controller/OrderController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import domain.menu.MenuRepository;
+import domain.order.OrderList;
+import domain.order.OrderProcessor;
+import view.InputOrderView;
+
 public class OrderController {
+    public OrderList run() {
+        InputOrderView inputOrderView = new InputOrderView();
+        return inputOrderView.getValue();
+    }
 }
